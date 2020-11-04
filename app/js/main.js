4fa30d0f727fe69487ab9a68d081023e9b96e3c4
moskitol89 <PptpRhad0ed95>
2020-11-04 23:11:40 +0300

    1.5 pages done

---
+++ app/js/main.js
@@ -0,0 +1,59 @@
+$(function () {
+    $('.image-popup-no-margins').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        showCloseBtn: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom',
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: false,
+        }
+    });
+    $(".gallery__item-links ul li:last-child a").click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass("clicked");
+    });
+
+    let reviewsSwiper = new Swiper('.reviews .swiper-container', {
+        spaceBetween: 30,
+        allowTouchMove: false,
+        autoHeight: true,
+       autoplay : {
+        delay: 5000
+       },
+        fadeEffect: {
+          crossFade: true
+        },
+        effect: 'fade',
+        navigation: {
+            nextEl: '.reviews .swiper-next',
+            prevEl: '.reviews .swiper-prev',
+            disabledClass: 'swiper-button-disabled'
+        },
+    });
+
+    $(".header__burger").click(function () {
+        $(".menu").slideToggle();
+    })
+    $(".header .menu ul li a").click(function () {
+        $(".menu").fadeToggle();
+    });
+
+    $(".process__tab").click(function (){
+        $(this).addClass("active").siblings().removeClass("active");
+        let index = $(".process__tab").index(this);
+        $(".process__tab-item").eq(index).addClass("active")
+            .siblings().removeClass("active");
+    });
+
+    $(".about__page-gift .gift__item").click(function () {
+        $(".about__page-gift .gift__item").removeClass("active");
+       $(this).addClass("active");
+       $(".about__page-gift .gift__item .gift__hover-block").html("Выбрать карту");
+       $(".about__page-gift .gift__item.active .gift__hover-block").html("Карта выбрана");
+    });
+});
